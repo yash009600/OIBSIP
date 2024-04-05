6565540c24e1bb6d6ca8cbb9bcf999c049b24805
yash009600 <117855427+yash009600@users.noreply.github.com>
2024-04-05 18:41:50 +0530

    first commit

---
+++ CALCULATOR/script.js
@@ -0,0 +1,31 @@
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((button)=>{
+    button.addEventListener('click', (e)=>{
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value =  string; 
+        }
+        else if(e.target.innerHTML == 'DEL')
+        {
+            string = string.slice(0, -1);
+            string = eval(string)
+            document.querySelector('input').value =  string; 
+        }
+        else if(e.target.innerHTML == '√')
+        {
+            string = math.sqrt(string);
+            document.querySelector('input').value =  string; 
+        }
+        else if(e.target.innerHTML == 'C')
+        {
+            string = "";
+            document.querySelector('input').value =  string; 
+        }
+        else{
+            console.log(e.target)
+            string = string+e.target.innerHTML;
+            document.querySelector('input').value =  string; 
+        }
+    })
+})
